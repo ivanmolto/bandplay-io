6cdf8729cceef4bafaf99663cee60e9e1a487a86
Ivan Molto <ivanmolto@gmail.com>
2020-11-11 09:29:25 +0100

    chore: Add Genre Tags to Publish

---
+++ src/AddGenreGrid.svelte
@@ -0,0 +1,15 @@
+<script>
+  import AddGenreItem from "./AddGenreItem.svelte";
+  export let customGenresStore;
+</script>
+
+{#if customGenresStore.length != 0}
+  <div class="mt-4">Genres</div>
+{/if}
+<div class="flex flex-wrap">
+  {#each customGenresStore as genre}
+    <AddGenreItem {genre} />
+  {:else}
+    <div />
+  {/each}
+</div>
---
+++ src/AddGenreItem.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let genre;
+</script>
+
+<div class="w-1/3 p-2">
+  <div class="bg-gray-400 text-gray-900 text-center p-2 rounded-lg truncate">
+    {genre}
+  </div>
+</div>
