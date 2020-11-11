99b401a45cdea33b70ae9009bdbcced744172154
Ivan Molto <ivanmolto@gmail.com>
2020-11-12 00:19:53 +0100

    chore: Delete Editor

--- src/App.svelte
+++ src/App.svelte
@@ -4,8 +4,10 @@
   import SearchFilters from "./SearchFilters.svelte";
   import Footer from "./Footer.svelte";
   import Home from "./Home.svelte";
-  import Editor from "./Editor.svelte";
-  import Track from "./Track.svelte";
+  // import Track from "./Track.svelte";
+  import Release from "./Release.svelte";
+  import Releases from "./Releases.svelte";
+  import Publish from "./Publish.svelte";
   import Pick from "./Pick.svelte";
   import Artist from "./Artist.svelte";
   import Library from "./Library.svelte";
@@ -25,8 +27,12 @@
     const path = window.location.hash.slice(1);
     txid = null;
     prevPage = page;
-    if (path.startsWith("/track")) {
+    /* if (path.startsWith("/track")) {
       page = "track";
+      txid = path.match(TX_PATTERN)[0];
+    } else  */
+    if (path.startsWith("/release")) {
+      page = "release";
       txid = path.match(TX_PATTERN)[0];
     } else if (path.startsWith("/pick")) {
       page = "pick";
@@ -50,9 +56,9 @@
         page = "home";
         window.location.href = "#";
       }
-    } else if (path.startsWith("/editor")) {
+    } else if (path.startsWith("/publish")) {
       if (isLoggedIn) {
-        page = "editor";
+        page = "publish";
       } else {
         page = "home";
         window.location.href = "#";
@@ -72,16 +78,18 @@
   {#if page === 'home'}
     <SearchFilters />
     <Home on:auth={onAuth} />
-  {:else if page === 'editor'}
-    <Editor {wallet} {address} />
+  {:else if page === 'publish'}
+    <Publish {wallet} {address} />
   {:else if page === 'account'}
     <Account {wallet} {address} />
   {:else if page === 'pick'}
-    <Pick {wallet} {address} {prevPage} {txid} />
+    <Pick {wallet} {address} {txid} />
   {:else if page === 'track'}
     <Track {wallet} {address} {prevPage} {txid} />
+  {:else if page === 'release'}
+    <Release {wallet} {address} {prevPage} {txid} />
   {:else if page === 'artist'}
-    <Artist {prevPage} {txid} />
+    <Artist {txid} />
   {:else if page === 'library'}
     <Library {address} />
   {/if}
