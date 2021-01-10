c8d958a7461482b60e4030d42dbc3d9f4a6a5a89
Ivan Molto <ivanmolto@gmail.com>
2021-01-10 11:54:31 +0100

    feat: Add Beats to Landing page

---
+++ src/LandingBeats.svelte
@@ -0,0 +1,88 @@
+<div class="max-w-screen-lg xl:max-w-screen-xl mx-auto mt-32">
+    <div class="px-4 sm:px-6 md:px-8 mb-14 sm:mb-20 xl:mb-8">
+      <div class="font-montserrat text-4xl font-semibold text-orange-700 mb-2">Share The Moment With Beats</div>
+      <p class="max-w-4xl text-lg sm:text-xl font-montserrat text-gray-400 sm:leading-8 space-y-6 mb-6">Get tips for sharing your unedited mixes, micro-podcasts, crazy moments...<br />Your music stories last forever.</p>
+      <ul class="flex space-x-6 font-montserrat text-gray-600">
+        <li class="flex flex-col items-center space-y-1">
+            <div class="relative bg-gradient-to-tr from-yellow-400 to-orange-700 p-1 rounded-full">
+                <a class="block bg-white p-1 rounded-full transform transition hover:-rotate-6" href="#">
+                    <img class="h-24 w-24 rounded-full" src="https://arweave.net/jtKeH9UqTgV_NCvvjHnEXNwyEcOtvuBrzxyv3OUK9Tc" alt="you" />
+                </a>
+                <button class="absolute bottom-0 right-1 bg-blue-500 h-8 w-8 rounded-full text-white text-2xl font-semibold border-4 border-white flex justify-center items-center font-mono hover:bg-blue-700">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" class="fill-current"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M18 13h-5v5c0 .55-.45 1-1 1s-1-.45-1-1v-5H6c-.55 0-1-.45-1-1s.45-1 1-1h5V6c0-.55.45-1 1-1s1 .45 1 1v5h5c.55 0 1 .45 1 1s-.45 1-1 1z"/></svg>
+                </button>
+            </div>
+            <a href="#">You</a>
+        </li>
+        <li class="flex flex-col items-center space-y-1">
+            <div class="bg-gradient-to-tr from-yellow-400 to-orange-700 p-1 rounded-full">
+                <a class="block bg-white p-1 rounded-full transform transition hover:-rotate-6" href="#">
+                    <img class="h-24 w-24 rounded-full" src="https://arweave.net/NsAHwlrhY7s6KzrwFR867-v_e_uv0DZ_slKJSAZxkmQ" alt="bankzik" />
+                </a>
+            </div>
+            <a href="#">bankzik</a>
+        </li>
+        <li class="flex flex-col items-center space-y-1">
+            <div class="bg-gradient-to-tr from-yellow-400 to-orange-700 p-1 rounded-full">
+                <a class="block bg-white p-1 rounded-full transform transition hover:-rotate-6" href="#">
+                    <img class="h-24 w-24 rounded-full" src="https://arweave.net/3zq6SWDxb59WlT5jbWigIYQ9MD4HfEPgkDy8A7tRY0M" alt="beckyh" />
+                </a>
+            </div>
+            <a href="#">beckyh</a>
+        </li>
+        <li class="hidden md:inline-flex flex-col items-center space-y-1">
+            <div class="bg-gradient-to-tr from-yellow-400 to-orange-700 p-1 rounded-full">
+                <a class="block bg-white p-1 rounded-full transform transition hover:-rotate-6" href="#">
+                    <img class="h-24 w-24 rounded-full" src="https://arweave.net/XB5kZtjrvMAL7IW0MmATpvXFLeh2BpJWkKTaRkHjfvo" alt="dreamer" />
+                </a>
+            </div>
+            <a href="#">dreamer</a>
+        </li>
+        <li class="hidden md:inline-flex flex-col items-center space-y-1">
+            <div class="bg-gradient-to-tr from-yellow-400 to-orange-700 p-1 rounded-full">
+                <a class="block bg-white p-1 rounded-full transform transition hover:-rotate-6" href="#">
+                    <img class="h-24 w-24 rounded-full" src="https://arweave.net/R8KA05_YjOcDm86x4uikO4q6ahYZ0UJcYXIZ6VdUuZE" alt="papichulo" />
+                </a>
+            </div>
+            <a href="#">papichulo</a>
+        </li>
+        <li class="hidden lg:inline-flex flex-col items-center space-y-1">
+            <div class="bg-gradient-to-tr from-yellow-400 to-orange-700 p-1 rounded-full">
+                <a class="block bg-white p-1 rounded-full transform transition hover:-rotate-6" href="#">
+                    <img class="h-24 w-24 rounded-full" src="https://arweave.net/Vt26ucplbOqnyi7OezlZ37TNBcEBuqCVgLb9So4vGXw" alt="hiphopster" />
+                </a>
+            </div>
+            <a href="#">hiphopster</a>
+        </li>
+        <li class="hidden lg:inline-flex flex-col items-center space-y-1">
+            <div class="bg-gradient-to-tr from-yellow-400 to-orange-700 p-1 rounded-full">
+                <a class="block bg-white p-1 rounded-full transform transition hover:-rotate-6" href="#">
+                    <img class="h-24 w-24 rounded-full" src="https://arweave.net/S8ojJA9Y4_LhjkasmW1OTOF-eskgBExMydeCgb93FZ4" alt="ivanmolto" />
+                </a>
+            </div>
+            <a href="#">ivanmolto</a>
+        </li>
+        <li class="hidden xl:inline-flex flex-col items-center space-y-1">
+            <div class="bg-gradient-to-tr from-yellow-400 to-orange-700 p-1 rounded-full">
+                <a class="block bg-white p-1 rounded-full transform transition hover:-rotate-6" href="#">
+                    <img class="h-24 w-24 rounded-full" src="https://arweave.net/F2EAeEr7nCDT5CHc85XrHthOiu4qMjLQFFUbqEt-SW0" alt="angelaluv" />
+                </a>
+            </div>
+            <a href="#">angelaluv</a>
+        </li>
+        <li class="hidden xl:inline-flex flex-col items-center space-y-1">
+            <div class="bg-gradient-to-tr from-yellow-400 to-orange-700 p-1 rounded-full">
+                <a class="block bg-white p-1 rounded-full transform transition hover:-rotate-6" href="#">
+                    <img class="h-24 w-24 rounded-full" src="https://arweave.net/WJzzRidu-XtOKDy0nseIOf5OmBvu1VTcHl1oxefOLSQ" alt="sabine" />
+                </a>
+            </div>
+            <a href="#">sabine</a>
+        </li>
+    </ul>
+    </div>
+    <div class="max-w-screen-lg xl:max-w-screen-xl mx-auto">
+        <div class="px-4 sm:px-6 md:px-8 mb-14 sm:mb-20 xl:mb-8 mt-12">
+            <a class="shadow-lg font-montserrat w-full sm:w-auto flex-none bg-gradient-to-r from-orange-500 to-orange-400 hover:from-orange-600 hover:to-orange-500 text-white font-semibold leading-6 py-3 px-6 border border-transparent rounded-lg focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-orange-900 focus:outline-none inline-flex items-baseline" href="#/home">Share Your Beats<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width="18px" height="18px" class="h-6 w-6 fill-current place-self-end ml-1"><path d="M0 0h24v24H0z" fill="none"/><path d="M10 20h4V4h-4v16zm-6 0h4v-8H4v8zM16 9v11h4V9h-4z"/></svg></a>
+        </div>
+    </div>
+</div>
